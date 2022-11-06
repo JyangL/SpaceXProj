2979cb75598f0822550126add71adbf953e66d7c
Jianyang Li <52183259+JyangL@users.noreply.github.com>
2022-11-06 13:24:03 -0600

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const SAT_API_KEY = "YYXPEW-8QXCS5-L7Y4YU-4UT4";
+export const SAT_API_KEY = "";
 export const STARLINK_CATEGORY = "52";
 export const NEARBY_SATELLITE = "rest/v1/satellite/above";
 export const WORLD_MAP_URL = "https://unpkg.com/world-atlas@1/world/110m.json";
